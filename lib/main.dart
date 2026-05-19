e96089ee48f413e59a52e829188c29983b909fce
queevvedoDyordan <dyordan.soares@gmail.com>
2026-05-19 20:56:19 -0300

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,25 @@
+import 'package:aula_15/pages/homepage.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage()
     );
   }
 }
